887d1f0280688d7a4e57bfe35e211c63f68311d7
kajikawa256 <kajikawa2003@gmail.com>
2023-01-05 02:09:49 +0900

    高速化を施したが変化なし

--- duplicationRate.py
+++ duplicationRate.py
@@ -1,13 +1,22 @@
-import time
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 
+#オプション
 options = webdriver.ChromeOptions()
+options.add_argument('d--disable-blink-features=AutomationControlled')
+options.add_argument('--blink-settings=imagesEnabled=false')
 options.add_argument('–headless')
 options.add_argument('–no-sandbox')
 options.add_argument('–disable-dev-shm-usage')
 
+#URLを設定
 URL = 'http://enworddojo.holy.jp/index.php'
+
+#高速化
+desired = DesiredCapabilities().CHROME
+desired['pageLoadStrategy'] = 'none'
 
 #Chromeを起動
 driver = webdriver.Chrome()
@@ -20,10 +29,13 @@ cur_url = driver.current_url
 #カレントページのURLを表示
 # print(cur_url)
 
-button = driver.find_element(By.CSS_SELECTOR,
+
+for _ in range(10):
+  elem = driver.find_element(By.CSS_SELECTOR,
 ("button[id='nextbutton2']"))
-driver.execute_script('arguments[0].click();',button)
-time.sleep(3)
+  driver.execute_script('arguments[0].click();',elem)
+  #1秒待機
+  # time.sleep(1)
 
 #Chromeを終了
 driver.quit()
