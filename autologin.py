9435705d88074a817d65681afd030387f76ab6e6
kajikawa256 <kajikawa2003@gmail.com>
2023-01-05 03:29:50 +0900

    自動ログインプログラム作成着手

---
+++ autologin.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import subprocess
+import time
+import os
+
+
+os.system('C:/Users/2220077/Desktop/language/python/utils/openChrome.bat')
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("debuggerAddress","127.0.0.1:9222")
+# driver = webdriver.Chrome(options=options)
+
+# print(driver.title)
+
+
